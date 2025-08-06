a9932dbcccda32ee38c0d518c142f2a24e2d8d8f
Zac <mohamadandrew589@gmail.com>
2025-08-06 09:35:52 +0700

    Create menu.js

---
+++ watsapp-bot/menu.js
@@ -0,0 +1,15 @@
+const { ownerName } = require('./config')
+
+module.exports = () => `
+🤖 *WhatsApp Bot Downloader*
+
+Pilih menu:
+
+1. Download nHentai
+   Format: nhentai 123456
+
+2. Download TikTok
+   Format: tiktok https://tiktok.com/...
+
+💻 ${ownerName}
+`
